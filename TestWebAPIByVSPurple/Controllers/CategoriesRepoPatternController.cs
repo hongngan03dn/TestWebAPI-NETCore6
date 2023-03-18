56428f2a8fed890196339e3421f5a0d5329394a3
Ngan <hongngan03dn@gmail.com>
2023-03-18 18:46:47 +0700

    Real CRUD DB apis

---
+++ TestWebAPIByVSPurple/Controllers/CategoriesRepoPatternController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TestWebAPIByVSPurple.Models;
+using TestWebAPIByVSPurple.Repositories;
+
+namespace TestWebAPIByVSPurple.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesRepoPatternController : ControllerBase
+    {
+        private readonly ICategoryRepository _repo;
+
+        public CategoriesRepoPatternController(ICategoryRepository repo) 
+        {
+            _repo = repo;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllCategories() 
+        {
+            try
+            {
+                return Ok(await _repo.GetAllCategoriesAsync());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCategoryByID(int id)
+        {
+            var cate = await _repo.GetCategoryAsync(id);
+            return cate == null ? NotFound() : Ok(cate);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddNewCategory(CategoryModel model)
+        {
+            try
+            {
+                var newId = await _repo.AddCategoryAsync(model);
+                var cate = await _repo.GetCategoryAsync(newId);
+                return cate == null ? NotFound() : Ok(cate);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateCategory(int id, CategoryModel model)
+        {
+            await _repo.UpdateCategoryAsync(id, model);
+            return Ok();
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCategory(int id)
+        {
+            await _repo.DeleteCategoryAsync(id);
+            return Ok();
+        }
+    }
+}
---
+++ TestWebAPIByVSPurple/Repositories/CategoryRepository.cs
@@ -0,0 +1,62 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using TestWebAPIByVSPurple.Entities;
+using TestWebAPIByVSPurple.Models;
+
+namespace TestWebAPIByVSPurple.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly MyShopContext _context;
+        private readonly IMapper _mapper;
+
+        public CategoryRepository(MyShopContext context, IMapper mapper) 
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<int> AddCategoryAsync(CategoryModel model)
+        {
+            try
+            {
+                model.Id = null;
+                var newCate = _mapper.Map<Category>(model);
+                _context.Categories.Add(newCate);
+
+                await _context.SaveChangesAsync();
+                return newCate.Id;
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+
+        public async Task DeleteCategoryAsync(int id)
+        {
+            var category = _context.Categories.Find(id);
+            _context.Categories.Remove(category);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<CategoryModel>> GetAllCategoriesAsync()
+        {
+            var categories = await _context.Categories.ToListAsync();
+            return _mapper.Map<List<CategoryModel>>(categories);
+        }
+
+        public async Task<CategoryModel> GetCategoryAsync(int id)
+        {
+            var category = await _context.Categories.FindAsync(id);
+            return _mapper.Map<CategoryModel>(category);
+        }
+
+        public async Task UpdateCategoryAsync(int id, CategoryModel model)
+        {
+            var category = _mapper.Map<Category>(model);
+            _context.Categories.Update(category);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
